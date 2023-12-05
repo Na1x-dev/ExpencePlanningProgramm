d5bff70c2ced34c467f7e3c8090402ec4e04268a
Na1x-dev <na1x.1024@gmail.com>
2023-12-05 11:48:46 +0300

    fix: changed server

---
+++ server/src/main/resources/database/populateDB.sql
@@ -0,0 +1,10 @@
+
+insert into roles (roleName) values ('заказчик');
+insert into roles (roleName) values ('исполнитель');
+insert into roles (roleName) values ('администратор');
+
+
+insert into statuses (status_name) values ('создано');
+insert into statuses (status_name) values ('зарегистрировано');
+insert into statuses (status_name) values ('отклонено');
+insert into statuses (status_name) values ('закрыто');
